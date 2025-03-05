e7bd4f64ddb92edd0f5b91a8dc28755fa035a387
RyPi <ryan.wemmer@gmail.com>
2025-03-05 05:22:23 -0700

    modified additional scripts for 2 way communication between pi and pico

--- Pico/Communications/pico-serial.py
+++ Pico/Communications/pico-serial.py
@@ -1,19 +1,23 @@
-import machine
 import utime
+import sys
+import select
 
+def send_message(message):
+    """Send a message to the USB serial connection (prints to stdout)."""
+    print(message)
+    
+
+def receive_message(timeout=0):
+    """
+    Check for and receive a message from the USB serial connection (stdin).
+    Optional timeout (seconds), 0 = non-blocking check.
+    """
+    if sys.stdin in select.select([sys.stdin], [], [], timeout)[0]:
+        return sys.stdin.readline().strip()
+    return None
 # Simple script to communicate between a pi and a pico
 # PICO Script
 # open a direct connection over USB
-uart = machine.UART(0, baudrate=9600, tx=machine.Pin(0), rx=machine.Pin(1))
-
-def send_message(message):
-    uart.write(message + "\n")
-
-def receive_message():
-    if uart.any():
-        message = uart.read().decode('utf-8').strip()
-        return message
-    return None
 
 while True:
     message = receive_message()
