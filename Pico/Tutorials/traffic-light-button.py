c14e9985ea5922aa489d6c5efd5c99e0f9434ca5
RyPi <ryan.wemmer@gmail.com>
2025-03-06 05:15:59 -0700

    Basic traffic light with button script added

---
+++ Pico/Tutorials/traffic-light-button.py
@@ -0,0 +1,44 @@
+# Traffic light game
+# https://docs.sunfounder.com/projects/thales-kit/en/latest/micropython/traffic_light.html
+import machine
+import utime
+import _thread
+
+led_red = machine.Pin(15, machine.Pin.OUT)
+led_yellow = machine.Pin(14, machine.Pin.OUT)
+led_green = machine.Pin(13, machine.Pin.OUT)
+button = machine.Pin(16, machine.Pin.IN)
+
+global button_status
+button_status = 0
+
+def button_thread():
+    global button_status
+    while True:
+        if button.value() == 1:
+            button_status = 1
+
+_thread.start_new_thread(button_thread, ())
+
+while True:
+    if button_status == 1:
+        led_red.value(1)
+        utime.sleep(10)
+    global button_status
+    button_status = 0
+
+    led_red.value(1)
+    utime.sleep(5)
+    led_red.value(0)
+
+    led_yellow.value(1)
+    utime.sleep(2)
+    led_yellow.value(0)
+
+    led_green.value(1)
+    utime.sleep(5)
+    led_green.value(0)
+
+    led_yellow.value(1)
+    utime.sleep(2)
+    led_yellow.value(0)
