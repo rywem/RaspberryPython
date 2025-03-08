5276210d750979e96c929defef75c90e73a3614a
RyPi <ryan.wemmer@gmail.com>
2025-03-08 06:16:49 -0700

    added comment to code file

--- Pico/Tutorials/transistors-simple.py
+++ Pico/Tutorials/transistors-simple.py
@@ -2,9 +2,10 @@ import machine
 button = machine.Pin(14, machine.Pin.IN)
 signal = machine.Pin(15, machine.Pin.OUT)
 
+# pressing the button should toggle the LED on and off
 while True:
     button_status = button.value()
     if button_status== 1:
-        signal.value(1)
+        signal.value(1)        
     elif button_status == 0:
         signal.value(0)
