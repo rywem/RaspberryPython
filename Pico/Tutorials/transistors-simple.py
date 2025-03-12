6c98129d5a003e5f2853edfce3c668406f025bb0
RyPi <ryan.wemmer@gmail.com>
2025-03-12 05:14:49 -0600

    removed test file

--- Pico/Tutorials/transistors-simple.py
+++ Pico/Tutorials/transistors-simple.py
@@ -6,6 +6,6 @@ signal = machine.Pin(15, machine.Pin.OUT)
 while True:
     button_status = button.value()
     if button_status== 1:
-        signal.value(1)        
+        signal.value(1)
     elif button_status == 0:
         signal.value(0)
