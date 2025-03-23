94978e32ebc1cddc02abb268bbb7ccbf136808d2
Ryan <cybervoid41@gmail.com>
2025-03-23 06:20:08 -0600

    fix pico script for pull up resistor

--- PiToPicoConversions/internal-pull-up.py
+++ PiToPicoConversions/internal-pull-up.py
@@ -1,18 +1,26 @@
 import machine
 import utime
 
-pinIn = machine.Pin(15, machine.Pin.IN)
-pinLight = machine.Pin(14, machine.Pin.OUT, Pin.PULL_UP)
+# Use internal pull-up on input pin
+pinIn = machine.Pin(15, machine.Pin.IN, machine.Pin.PULL_UP)
+
+# Set output pin (no pull-up needed for output)
+pinLight = machine.Pin(14, machine.Pin.OUT)
 
 try:
     while True:
-        readVal = pinIn.value()
-        if readVal == 0:
-            pinLight.value(1)
-        else:
-            pinLight.value(0)
+        readVal = pinIn.value()  # Will be 1 when NOT pressed (pulled up), 0 when pressed (grounded)
         print(readVal)
-        utime.sleep(.3)
+
+        if readVal == 0:
+            pinLight.value(1)  # Turn on light
+        else:
+            pinLight.value(0)  # Turn off light
+
+        utime.sleep(0.3)
+
 except KeyboardInterrupt:
+    # Not strictly needed on Pico, but you can reset output pin as input if you want to 'clean up'
     pinIn = machine.Pin(15, machine.Pin.IN)
     pinLight = machine.Pin(14, machine.Pin.IN)
+    print("Pins reset")
