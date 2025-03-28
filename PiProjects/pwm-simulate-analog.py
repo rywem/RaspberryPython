d04d0e4810541026025ed80358ff7f17202804b6
Ryan <cybervoid42@gmail.com>
2025-03-28 03:23:14 -0600

    stub for script brighter-dimmer-buttons

--- PiProjects/pwm-simulate-analog.py
+++ PiProjects/pwm-simulate-analog.py
@@ -7,18 +7,18 @@ GPIO.setup(37, GPIO.OUT)
 
 
 try:
-    GPIO.output(37, True)
-    sleep(.5)
-    GPIO.output(37, False)
-    sleep(.1)
+#    GPIO.output(37, True)
+ #   sleep(.5)
+ #   GPIO.output(37, False)
+  #  sleep(.1)
     myPWM = GPIO.PWM(37, 100) #frequency
-    myPWM.start(10) #duty cycle in %
-    #myPWM.ChangeDutyCycle(50)
+    myPWM.start(50) #duty cycle in %
+    myPWM.ChangeDutyCycle(10)
     myPWM.ChangeFrequency(10)
     
     #while True:
      #   GPIO.output(37, True)
-    sleep(2)
+    sleep(5)
     myPWM.stop()
 except KeyboardInterrupt:
     GPIO.cleanup()
