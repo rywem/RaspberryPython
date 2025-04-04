a1d2a88137328ed127398b1f88b10dceafd2d76c
Ryan <cybervoid42@gmail.com>
2025-04-04 03:31:54 -0600

    pico equivalent started for rgb and button press script

---
+++ PiToPicoConversions/pico-rgb-buttons.py
@@ -0,0 +1,33 @@
+#pico-rgb-buttons.py
+from machine import Pin
+from time import sleep
+import utils
+rPin = Pin(13, Pin.OUT)
+gPin = Pin(12, Pin.OUT)
+bPin = Pin(11, Pin.OUT)
+
+rButton = Pin(36, Pin.IN, Pin.PULL_DOWN)
+gButton = Pin(38, Pin.IN, Pin.PULL_DOWN)
+bButton = Pin(40, Pin.IN, Pin.PULL_DOWN)
+
+try:
+    while True:
+        rPin.value(False)
+        gPin.value(False)
+        bPin.value(False)
+        
+        if rButton.value() == True
+            rPin.value(True)
+            print("Red Pressed!")
+        if gButton.value() == True
+            gPin.value(True)
+            print("Green Pressed!")
+        if bButton.value() == True
+            bPin.value(True)
+            print("Blue Pressed!")
+except:
+    print("interrupted pico")
+
+utils.pin_cleanup(rPin)
+utils.pin_cleanup(gPin)
+utils.pin_cleanup(bPin)
