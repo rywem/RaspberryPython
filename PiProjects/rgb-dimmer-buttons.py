88f88bbfd12ec7e48c9a3e94c680c30cea68225d
Ryan <cybervoid42@gmail.com>
2025-04-06 05:49:37 -0600

    script created, will increase or decrease color intensity, with a button to toggle the mode between increase and decrease mode

--- PiProjects/rgb-dimmer-buttons.py
+++ PiProjects/rgb-dimmer-buttons.py
@@ -12,7 +12,7 @@ bLedPin = 33
 rButton = 36
 gButton = 38
 bButton = 40
-
+modeButton = 32
 GPIO.setup(rLedPin, GPIO.OUT)
 GPIO.setup(gLedPin, GPIO.OUT)
 GPIO.setup(bLedPin, GPIO.OUT)
@@ -24,7 +24,9 @@ GPIO.setup(bLedPin, GPIO.OUT)
 GPIO.setup(rButton, GPIO.IN,  pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(gButton, GPIO.IN,  pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(bButton, GPIO.IN,  pull_up_down=GPIO.PUD_DOWN)
-
+GPIO.setup(modeButton, GPIO.IN,  pull_up_down=GPIO.PUD_DOWN)
+modeState = 1
+modeStateOld = 1
 rState = 0
 gState = 0
 bState = 0
@@ -57,28 +59,44 @@ bDutyCycleOld = 1.01
 rPWM.start(rDutyCycle)
 gPWM.start(gDutyCycle)
 bPWM.start(bDutyCycle)
-
+stateChangeType = "Increase"
 try:
     
     while True:
-        #GPIO.output(rLedPin, False)
-        #GPIO.output(gLedPin, False)
-        #GPIO.output(bLedPin, False)
         rState = GPIO.input(rButton)
         gState = GPIO.input(gButton)
-        bState = GPIO.input(bButton)     
+        bState = GPIO.input(bButton)
+        modeState = GPIO.input(modeButton)
+        if modeState == 1:
+            modeStateOld = not modeStateOld
+            if modeStateOld == 1:
+                print("Mode State: Increase")
+                stateChangeType = "Increase"
+            else:
+                print("Mode State: Decrease")
+                stateChangeType = "Decrease"
         if rState == 1:
-            rBP += 1
+            if modeStateOld == 1:
+                rBP += 1
+            else:
+                rBP -= 1                
             rDutyCycle = (1.5849) ** rBP
-            print("red pressed")
+            print("Red: ", stateChangeType)
         if gState == 1:
-            gBP += 1
+            if modeStateOld == 1:
+                gBP += 1
+            else:
+                gBP -= 1                            
             gDutyCycle = (1.5849) ** gBP
-            print("Green pressed")
+            print("Green: ", stateChangeType)
         if bState == 1:
-            bBP += 1
+            if modeStateOld == 1:
+                bBP += 1
+            else:
+                bBP -= 1                            
             bDutyCycle = (1.5849) ** bBP
-            print("Blue pressed")
+            print("Blue: ", stateChangeType)
+            
         rBP = max(min(rBP, 10), 1)
         gBP = max(min(gBP, 10), 1)
         bBP = max(min(bBP, 10), 1)
@@ -99,7 +117,7 @@ try:
             bDutyCycleOld = bDutyCycle
             print("Blue Duty Cycle:", bDutyCycle)
             
-        sleep(.2)
+        sleep(.15)
         
 except KeyboardInterrupt:
     print("cleanup complete.")
