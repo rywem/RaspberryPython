0ebd1449a8b057c724eb60200b3cfca53d5d1e70
Ryan <cybervoid42@gmail.com>
2025-04-12 04:49:11 -0600

    basic script to interrupt potentiometer results using analog circuit

--- PiProjects/analog-inputs.py
+++ PiProjects/analog-inputs.py
@@ -3,3 +3,18 @@
 
 # using analog to digital converter
 import ADC0834
+import RPi.GPIO as GPIO
+from time import sleep
+
+GPIO.setmode(GPIO.BCM)
+#initialize the chip
+ADC0834.setup()
+
+try:
+    while True:
+        analogValue = ADC0834.getResult(0) #pass channel as a parameter
+        print(analogValue)
+        sleep(.2)
+except KeyboardInterrupt:
+    GPIO.cleanup()
+    print('GPIO Cleaned and exited')
