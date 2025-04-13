9bfcf7e38396ebc2c346c84f70b33b7ca96822b1
Ryan <cybervoid42@gmail.com>
2025-04-13 12:41:00 -0600

    potentiometer will now increase or decrease the brightness (via duty cycle modification) of an LED

--- PiProjects/analog-inputs-LED-dimmer.py
+++ PiProjects/analog-inputs-LED-dimmer.py
@@ -10,10 +10,20 @@ GPIO.setmode(GPIO.BCM)
 #initialize the chip
 ADC0834.setup()
 
+ledPin = 20
+GPIO.setup(ledPin, GPIO.OUT)
+
+rPWM = GPIO.PWM(ledPin,100)
+dutyCycle = 1
+rPWM.start(dutyCycle)
 try:
     while True:
         analogValue = ADC0834.getResult(1) #pass channel as a parameter
-        
+        if analogValue == 0:
+            dutyCycle = 1
+        else:
+            dutyCycle = (analogValue/255) * 100
+        rPWM.ChangeDutyCycle(dutyCycle)
         print(analogValue)
         sleep(.2)
 except KeyboardInterrupt:
