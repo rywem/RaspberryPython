e2f108435564c49af6f133444d44b009b26d6764
Ryan <cybervoid42@gmail.com>
2025-04-14 05:40:12 -0600

    homework version of paul mcwhorter solution to building a dimmable led with a potentiometer and integrated circuit adc 0834

--- PiProjects/pw-analog-inputs-LED-dimmer.py
+++ PiProjects/pw-analog-inputs-LED-dimmer.py
@@ -4,7 +4,7 @@ from time import sleep
 
 # Setup variables
 delayTime = .1
-redPin = 16
+redPin = 20
 dutyCycle = 0
 
 # Setup board
@@ -15,8 +15,32 @@ GPIO.setup(redPin, GPIO.OUT)
 pwm = GPIO.PWM(redPin, 1000)
 pwm.start(dutyCycle)
 
+'''
+Calculate a slope:
+(0,0) [255, 100]
+
+m = (y2 - y1) / (x2 - x1)
+m = (100 - 0) / (255 - 0)
+m = 100 / 255 (slope)
+(y - y1) = m(x - x1)
+y = (100 / 255)*x
+(x = potVal)
+y = Duty Cycle
+'''
+
 try:
     while True:
+        # read value from potentiometer
+        potVal = ADC0834.getResult(0)
+        if potVal == 0:
+            dutyCycle = 0
+        else:
+            dutyCycle = (100/255) * potVal            
+        if dutyCycle > 99:
+            dutyCycle = 99        
+        pwm.ChangeDutyCycle(dutyCycle)
+        print(potVal, dutyCycle)
+        sleep(delayTime)
         
 except KeyboardInterrupt:
     pwm.stop()
